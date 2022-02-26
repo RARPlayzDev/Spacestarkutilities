3c633c1d2a9738e1057e81a2b9b8fc306b657b97
RARPlayzDev <kaarthikeyanganesh@gmail.com>
2022-02-26 13:41:20 +0530

    Custom Item Tab

--- src/main/java/net/rarplayz/spacestark/item/ModCreativeModeTab.java
+++ src/main/java/net/rarplayz/spacestark/item/ModCreativeModeTab.java
@@ -4,7 +4,7 @@ import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 
 public class ModCreativeModeTab {
-    public static final CreativeModeTab SPACE_TAB = new CreativeModeTab("SpaceStark Utils") {
+    public static final CreativeModeTab SPACE_TAB = new CreativeModeTab("Stark Utils") {
         @Override
         public ItemStack makeIcon() {
             return new ItemStack(ModItems.CITRINE.get());
