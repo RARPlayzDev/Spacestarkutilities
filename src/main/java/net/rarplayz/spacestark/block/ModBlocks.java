bf1c56587d39f17aa502a462c83f15efb0c734f5
RARPlayzDev <kaarthikeyanganesh@gmail.com>
2022-02-28 13:30:11 +0530

    Added Tags

--- src/main/java/net/rarplayz/spacestark/block/ModBlocks.java
+++ src/main/java/net/rarplayz/spacestark/block/ModBlocks.java
@@ -1,8 +1,9 @@
 package net.rarplayz.spacestark.block;
 
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.item.*;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
@@ -14,7 +15,9 @@ import net.rarplayz.spacestark.Spacestark;
 import net.rarplayz.spacestark.block.custom.SpeedyBlock;
 import net.rarplayz.spacestark.item.ModCreativeModeTab;
 import net.rarplayz.spacestark.item.ModItems;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 public class ModBlocks {
@@ -46,24 +49,42 @@ public class ModBlocks {
                 () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
                         .strength(5f).requiresCorrectToolForDrops()), ModCreativeModeTab.SPACE_TAB);
 
-        public static final RegistryObject<Block> SPEEDY_BLOCK = registerBlock("speedy_block",
+    public static final RegistryObject<Block> SPEEDY_BLOCK = registerBlock("speedy_block",
             () -> new SpeedyBlock(BlockBehaviour.Properties.of(Material.STONE)
-                    .strength(4f).requiresCorrectToolForDrops()), ModCreativeModeTab.SPACE_TAB);
+                    .strength(4f).requiresCorrectToolForDrops()), ModCreativeModeTab.SPACE_TAB, "tooltip.spacestark.block.speedy_block");
 
 
-        private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
-            RegistryObject<T> toReturn = BLOCKS.register(name, block);
-            registerBlockItem(name, toReturn, tab);
-            return toReturn;
-        }
-
-        private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block,
-                                                                                CreativeModeTab tab) {
-            return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(),
-                    new Item.Properties().tab(tab)));
-        }
-
-        public static void register(IEventBus eventBus) {
-            BLOCKS.register(eventBus);
-        }
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block,
+                                                                     CreativeModeTab tab, String tooltipKey) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn, tab, tooltipKey);
+        return toReturn;
     }
+
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block,
+                                                                            CreativeModeTab tab, String tooltipKey) {
+        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(),
+                new Item.Properties().tab(tab)) {
+            @Override
+            public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltip, TooltipFlag pFlag) {
+                pTooltip.add(new TranslatableComponent(tooltipKey));
+            }
+        });
+    }
+
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn, tab);
+        return toReturn;
+    }
+
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block,
+                                                                            CreativeModeTab tab) {
+        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(),
+                new Item.Properties().tab(tab)));
+    }
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
+}
